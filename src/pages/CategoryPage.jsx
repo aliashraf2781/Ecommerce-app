c9bf32a3e2ae5ed2ca678efe602915cc68e0a0cd
Ali Ashraf <aaaaaa30101975@gmail.com>
2025-07-23 21:19:58 +0300

    fix product card

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,32 +1,22 @@
 import { useParams } from "react-router-dom";
 import { useFilteredProducts } from "../hooks/useFilteredProducts";
 import ProductList from "../components/Product/ProductList";
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'
+import SkeletonLoader from "../components/Skeleton/SkeletonLoader";
+
 
 export default function CategoryPage() {
     const { categoryId } = useParams();
 
     const { data: products, isLoading } = useFilteredProducts({ category: categoryId });
-    console.log(products, isLoading);
 
-    if (isLoading) {
-        return (
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-                <SkeletonTheme baseColor="#f0f0f0" highlightColor="#e0e0e0">
-                    {Array.from({ length: 8 }).map((_, index) => (
-                        <Skeleton key={index} height={200} />
-                    ))}
-                </SkeletonTheme>
-            </div>
-        );
-    }
 
 
     return (
         <div className="p-4 bg-background ">
-            <h2 className="text-2xl font-bold mb-4 text-center text-dark"> {products[0].category?.nameEn}</h2>
-            {<ProductList products={products} isLoading={isLoading} />}
+            <h2 className="text-2xl font-bold mb-4 text-center text-dark">
+                {products?.[0]?.category?.nameEn || "Category Name"}
+            </h2>
+            {products?.length > 0 ? <ProductList products={products} /> : isLoading ? <SkeletonLoader isLoading={isLoading}><ProductList products={products} /></SkeletonLoader> : <div className='text-5xl text-center py-14'>No products available</div>}
         </div>
     );
 }
